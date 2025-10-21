7a8b9baaee0b2052ecc564083da297bb1e12c69d
Rausz Ádám <retroperitonealis@gmail.com>
2025-10-21 14:09:48 +0200

    Create Models folder and add classes

---
+++ HotelBookingAPI/Models/Booking.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelBookingAPI.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+        public string GuestName { get; set; } = null!;
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
---
+++ HotelBookingAPI/Models/Room.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelBookingAPI.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
